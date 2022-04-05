24d2a7cb82a0610ad34a41d1015675a6d859c9e2
twaxlol <45589745+twaxlol@users.noreply.github.com>
2022-04-05 22:26:58 +0200

    z

---
+++ ProductOrder.Application/Validation/OrderValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using ProductOrder.Application.Commands;
+using ProductOrder.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOrder.Application.Validation
+{
+    public class OrderValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public OrderValidator()
+        {
+            RuleForEach(x => x.Products).SetValidator(new ProductForOrderValidator());
+        }
+    }
+    public class ProductForOrderValidator : AbstractValidator<ProductOrderModel>
+    {
+        public ProductForOrderValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Quantity).GreaterThanOrEqualTo(1);
+        }
+    }
+}
